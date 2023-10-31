8e4417b748af76aea87e7e0f142fac9b6165cf8d
sangeethakloudone <142477391+sangeethakloudone@users.noreply.github.com>
2023-11-01 00:34:34 +0530

    api documentation done

--- config.rb
+++ config.rb
@@ -57,7 +57,7 @@ end
 # Deploy Configuration
 # If you want Middleman to listen on a different port, you can set that below
 set :port, 4567
-
+set :encoding, 'utf-8'
 helpers do
   require './lib/toc_data.rb'
 end
